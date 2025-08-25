2b7f7f94e403f6899ce4aecc9bed233b341f4255
Siddharth Baleja <siddharthbaleja31@gmail.com>
2025-08-25 10:38:09 +0530

    Demo05

--- SOLID/ex05/src/Rectangle.java
+++ SOLID/ex05/src/Rectangle.java
@@ -1,6 +1,6 @@
-public class Rectangle {
+public class Rectangle implements Shape{
     protected int w,h;
     void setWidth(int w){ this.w = w; }
     void setHeight(int h){ this.h = h; }
-    int area(){ return w*h; }
+    public int area(){ return w*h; }
 }
--- SOLID/ex05/src/Square.java
+++ SOLID/ex05/src/Square.java
@@ -1,5 +1,11 @@
+public class Square implements Shape {
+    int side;
+    void setSide(int side){
+        this.side = side;
+    }
 
-public class Square extends Rectangle {
-    @Override void setWidth(int w){ this.w = this.h = w; }
-    @Override void setHeight(int h){ this.w = this.h = h; }
+    @Override
+    public int area() {
+        return side * side;
+    }
 }
